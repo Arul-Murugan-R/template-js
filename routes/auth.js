3df6b951bbdc5015b442694514e045df49cfb2bb
Arul-Murugan-R <arulmuruganarjun14@gmail.com>
2022-02-11 15:58:55 +0530

    signup validator added

--- routes/auth.js
+++ routes/auth.js
@@ -3,6 +3,7 @@ const express = require('express');
 const {body,validationResult} = require('express-validator/check')
 
 const route = express.Router();
+const User = require('../models/user')
 
 route.use('/login',(req,res,next)=>{
     res.render('login',{
@@ -12,11 +13,36 @@ route.use('/login',(req,res,next)=>{
     })
 })
 
-route.use('/signup',(req,res,next)=>{
+route.post('/signup',
+[
+    body('name').not().isEmpty().isLength({max:4}).withMessage('Check For the size of name'),
+    body('email').not().isEmail().custom((val,{req})=>{
+        User.find({email:val})
+        .then((stat)=>{
+            if(stat){
+                return Promise.reject('Email Id already taken')
+            }
+        })
+    }),
+    body('password').not().isEmpty().isLength({max:7}).withMessage('Weak Password Less Size'),
+],
+(req,res,next)=>{
+    console.log(req.body)
+    const errors = validationResult(req)
+    if(!errors.isEmpty()){
+        res.render('signup',{
+            path:req.originalUrl,
+            title:'SignUp Page',
+            message:errors.array()[1],
+        })
+    }
+})
+
+route.get('/signup',(req,res,next)=>{
     res.render('signup',{
         path:req.originalUrl,
         title:'SignUp Page',
-        
+        message:null
     })
 })
 
